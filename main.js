e8a3251ffa92b038772c8b0bd3614f2d693ff200
dorong0328 <qoralstmd6825@gmail.com>
2023-06-01 15:02:52 +0900

    Javascript 수정

--- main.js
+++ main.js
@@ -1,13 +1,18 @@
 const button = document.getElementById("button");
+const button2 = document.getElementById("button2");
 const menu = document.getElementById("menu");
+const menu2 = document.getElementById("menu2");
 
 button.onclick = function () {
+    if(menu2.classList.contains("show")) {
+        menu2.classList.remove("show");
+    }
     menu.classList.toggle("show");
-    // if(!(menu.classList.contains("show"))) {
-    //     menu.classList.add("show");
-    //     alert('test');
-    // } else {
-    //     alert("test2");
-    //     menu.classList.remove("show")
-    // }
 };
+
+button2.onclick = function () {
+    if(menu.classList.contains("show")) {
+        menu.classList.remove("show");
+    }
+    menu2.classList.toggle("show");
+}
